f247fc9ad333229bb31180d8315e15a197adec07
160709-sarah <sarah.lima2@estudante.ifms.edu.br>
2026-08-22 21:24:49 +0000

    add - exercicio05

---
+++ aula03/src/componentes/ListaUsuarios.jsx
@@ -0,0 +1,13 @@
+export default function ListaUsuarios({ usuarios }) {
+    return (
+        <ul>
+            {
+                usuarios.map(usuario => (
+                    <li key={usuario.id}>
+                        {usuario.name}
+                    </li>
+                ))
+            }
+        </ul>
+    )
+}
